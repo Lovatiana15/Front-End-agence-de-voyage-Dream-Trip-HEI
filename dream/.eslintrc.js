0343b04fa402360e43a6780ba3c401a818498845
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-02-04 15:51:11 +0300

    feat: add material & emotion & eslint

---
+++ dream/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'standard',
+        'plugin:react/recommended'
+    ],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [
+                '.eslintrc.{js,cjs}'
+            ],
+            parserOptions: {
+                sourceType: 'script'
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: [
+        'react'
+    ],
+    settings: {
+        react: {
+            version: '18.2.0'
+        }
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'react/prop-types': 0
+    }
+};
