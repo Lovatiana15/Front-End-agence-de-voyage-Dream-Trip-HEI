223911036e4a3bd9fe37bacc39f134dabee1c254
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-02-05 19:13:01 +0300

    fix : logo & menu

---
+++ dream/src/components/navbar/AboutUsBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function AboutUsBar() {
+    return (
+        <div>
+            <Button variant='contained' sx={{ backgroundColor: 'rgb(206, 141, 20)'}}>ABOUT US</Button>
+        </div>
+    );
+};
+
+export default AboutUsBar;
