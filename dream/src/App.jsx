bd6b5fe2e996f89249ac71fd33c984b2e886f8de
rado <hei.rado.2@gmail.com>
2024-02-15 14:33:01 +0300

    fix: export footer

--- dream/src/App.jsx
+++ dream/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
                 <Route path="/" element={<Home />}/>
             </Routes>
         </Router>
+        
     );
 }
 
--- dream/src/pages/Home/Home.jsx
+++ dream/src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from '../../components/navbar/NavMenu';
 import Header from './Header';
+import Footer from '../Footer/Footer';
 
 function Home() {
     return (
         <div >
             <Navbar />
             <Header/>
+            <Footer />
         </div>
+
     );
 };
 
