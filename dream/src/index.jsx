d578ceb6364765b543e557f2a78a2d169329f35d
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-02-15 22:17:42 +0300

    feat: add navbar for user

--- dream/src/index.jsx
+++ dream/src/index.jsx
@@ -6,9 +6,9 @@ import '../src/pages/Footer/Footer.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
