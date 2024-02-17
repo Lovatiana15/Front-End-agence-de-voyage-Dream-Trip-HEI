ed5317cd1a32699303406a1026ced817239dd390
rado <hei.rado.2@gmail.com>
2024-02-17 11:55:28 +0300

    fix: add endpoint to backend

--- dream/src/pages/login/login.jsx
+++ dream/src/pages/login/login.jsx
@@ -30,7 +30,7 @@ function Login({ handleSignupModalOpen, handleLoginModalClose }) {
             key: adminKey
         };
 
-        axios.post('http://localhost:8080/api/v1/user/login', userCredentials)
+        axios.post('https://backend-production-d4e6.up.railway.app/', userCredentials)
             .then(response => {
                 if (response.status === 200 && response.data.status) {
                     navigate('/home');
